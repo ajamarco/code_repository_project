f2097bdd25f0984a30e98af5c7903fac5d93303a
musta7188 <musta7188@hotmail.it>
2020-03-05 15:15:24 +0000

    css style for show page done

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,5 +11,14 @@ class Post < ApplicationRecord
     validates :title, :length => {minimum: 5}
     validates :description, :length => {minimum: 20}
     validates :content, :length => {minimum:20}
+
+    def show_tags
+        return_string = ""
+        tags = self.tags
+        tags.each do |tag|
+            return_string += "#{tag.name}, "
+        end
+        return_string.strip.chop #getting rid of the last whitespace and the last , using chop
+    end
 end
 
