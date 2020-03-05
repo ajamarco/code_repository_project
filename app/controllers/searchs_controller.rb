5c8099d9fa89803dad8b34e8bed90868e07c7578
alexandre jamarco <alexandre.j@gmail.com>
2020-03-05 09:31:03 +0000

    created search button

---
+++ app/controllers/searchs_controller.rb
@@ -0,0 +1,7 @@
+class SearchsController < ApplicationController
+
+    def search_posts
+        byebug
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :posts do 
-  resources :likes, only: [:create, :new, :destroy]
+    resources :likes, only: [:create, :new, :destroy]
   end
 
-
+  root 'sessions#home'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
- post '/posts/:id' => 'posts#add_comment'
-  root 'sessions#home'
+  post '/posts/:id' => 'posts#add_comment'
+  post '/search/posts' => 'searchs#search_posts'
 
 end
